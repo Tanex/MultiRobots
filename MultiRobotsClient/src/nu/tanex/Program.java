a19156fb860099d4015e2543a56b6538a89480cb
Victor Hedlund <hedlund.victor@gmail.com>
2015-11-26 11:51:55 +0100

    Initial commit

---
+++ MultiRobotsClient/src/nu/tanex/Program.java
@@ -0,0 +1,9 @@
+package nu.tanex;
+
+import nu.tanex.ioengines.*;
+
+public class Program {
+    public static void main(String[] args) {
+        HelloWorld.helloWorld();
+    }
+}
---
+++ MultiRobotsEngine/src/nu/tanex/ioengines/HelloWorld.java
@@ -0,0 +1,7 @@
+package nu.tanex.ioengines;
+
+public class HelloWorld {
+    public static void helloWorld(){
+        System.out.println("Hello World!");
+    }
+}
